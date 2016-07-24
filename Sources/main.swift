b068ee99782eebc541525d77f524cc1044f53fb4
Renzo Crisóstomo <renzo.crisostomo@xing.com>
2016-07-24 17:52:43 +0200

    Add implementation

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,14 @@
-print("Hello, world!")
+import Quicksort
+import Foundation
+
+print("Please input the array as a CSV")
+let csv = readLine(strippingNewline: true)
+guard let input = csv, !input.isEmpty else { exit(42) }
+var inputArray: [Int] = input.components(separatedBy: ",").map {
+  guard let casted = Int($0) else { exit(42) }
+  return casted
+}
+print("Input read: \(inputArray)")
+
+let sorted = Quicksort.sort(array: &inputArray)
+print("Sorted: \(sorted)")
